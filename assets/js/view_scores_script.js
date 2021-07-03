0ebda208eb0553b43bd9e7a8161adfa8eefb1080
Vish O <v.opatha@gmail.com>
2021-07-03 18:16:44 +0930

    Add manupulation to local storage in view_highscore.html

--- assets/js/view_scores_script.js
+++ assets/js/view_scores_script.js
@@ -1,15 +1,40 @@
 var previousBtn = document.querySelector("#previousBtn");
 var clearBtn = document.querySelector("#clearBtn");
-
+var scoreList = [];
 
  function visitMainPage(event){
     event.preventDefault();
     window.location.replace('index.html');
  }
 
+ // Clear local storage
  function clearHighScore(event){
      event.preventDefault();
-     
+
+     var existingList = localStorage.getItem("uScore");
+     console.log(existingList);
+
+    if (existingList == null) {
+        return;
+    }
+
+    else {
+        localStorage.removeItem("uScore");
+    }      
  }
 
+//  function init() {
+//     // Get stored todos from localStorage
+//     var storedScore = JSON.parse(localStorage.getItem('todos'));
+  
+//     // If todos were retrieved from localStorage, update the todos array to it
+//     if (storedTodos !== null) {
+//       todos = storedTodos;
+//     }
+  
+//     // This is a helper function that will render todos to the DOM
+//     renderTodos();
+//   }
+
  previousBtn.addEventListener("click",visitMainPage);
+ clearBtn.addEventListener("click",clearHighScore);
